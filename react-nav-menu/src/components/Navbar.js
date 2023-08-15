89add18094fcd132c46e717d2d28cdf93befa580
jos3ph-m <jmedina1985@hotmail.com>
2023-08-15 10:21:43 -0700

    Creating navbar component

--- react-nav-menu/src/components/Navbar.js
+++ react-nav-menu/src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 // rrd
 import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
@@ -9,3 +11,11 @@ import "./Navbar.css"
 import {GiRocketThruster} from "react-icons/gi"
 import {FaBars, FaTimes} from "react-icons/fa"
 import {IconContext} from "react-icons/lib"
+
+function Navbar() {
+    return (
+        <>
+            
+        </>
+    )
+}
